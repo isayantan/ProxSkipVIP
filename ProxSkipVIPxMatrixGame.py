f485d48073f2f9ae92993dee6a7c992d6c891ef6
Sayantan Choudhury <sayantan.choudhury4@gmail.com>
2023-11-15 01:29:12 +0400

    Created ProxSkipVIPxMatrixGame.py

---
+++ ProxSkipVIPxMatrixGame.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 23:21:13 2023
+
+@author: sayantanchoudhury
+"""
+
+import numpy as np
+from ProximalOperator import projection_simplex_sort
+from MatrixGame import MatrixGame
+
+
+
